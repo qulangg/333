da5a512b08cdb15c917284bac4682d2e1e2a81d1
qulangg <114425343+qulangg@users.noreply.github.com>
2022-10-28 08:18:47 +0800

    Update keymap_plain.c

--- keyboard/pointtest/keymap_plain.c
+++ keyboard/pointtest/keymap_plain.c
@@ -22,17 +22,17 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP(
         ESC, GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,   BSLS, \
-        CAPS,     A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
-        LSFT,        Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT, FN0, \
-        LCTL,       LGUI,LALT,             SPC,               RALT, RGUI,     RCTL),
+//        TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,   BSLS, \
+//        CAPS,     A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+//        LSFT,        Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH, RSFT, FN0, \
+//        LCTL,       LGUI,LALT,             SPC,               RALT, RGUI,     RCTL),
     /* 1: Fn Layer */
     KEYMAP(
         ESC, GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-        TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,  BSLS,  \
-        CAPS,    A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,   ENT,    \
-        LSFT,      Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,  RSFT,  FN0, \
-        LCTL,      LGUI,LALT,             SPC,               RALT, RGUI,     RCTL),
+//        TAB,   Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,  BSLS,  \
+ //       CAPS,    A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,   ENT,    \
+ //       LSFT,      Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,  RSFT,  FN0, \
+  //      LCTL,      LGUI,LALT,             SPC,               RALT, RGUI,     RCTL),
 };
 const action_t fn_actions[] = {
     /* Poker Layout */
